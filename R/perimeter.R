37e921f504d5c5571e26c6a6cf08a89b1b4aa33d
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2024-02-21 21:30:02 +0000

    version 0.2-14

--- R/perimeter.R
+++ R/perimeter.R
@@ -1,10 +1,10 @@
-#' compute perimeter from polygons or other geometries
+#' Compute perimeter from polygons or other geometries
 #' 
 #' @name perimeter
 #' @param x object of class \code{sf}, \code{sfc} or \code{sfg}
 #' @return numerical vector with perimeter for each feature (geometry), with unit of measure when possible
 #' @export
-st_perimeter = function(x) {
+st_perimeter_lwgeom = function(x) {
 	if (isTRUE(st_is_longlat(x)))
 		stop("for perimeter of longlat geometry, cast to LINESTRING and use st_length") # nocov
 	ret = CPL_perimeter(st_geometry(x), FALSE)
@@ -12,8 +12,21 @@ st_perimeter = function(x) {
 	ret
 }
 
+#' compute perimeter from polygons or other geometries
+#' 
+#' Deprecated.
+#' Use \code{sf::st_perimeter()} or \code{st_perimeter_lwgeom()} instead.
+#' @keywords internal
+#' @name perimeter-deprecated
+#' @param x object of class \code{sf}, \code{sfc} or \code{sfg}
+st_perimeter = function(x) {
+  .Deprecated("sf::st_perimeter or lwgeom::st_perimeter_lwgeom")
+  # for back compatibility 
+  st_perimeter_lwgeom(x)
+}
+
 #' @export
-#' @name perimeter
+#' @rdname perimeter
 st_perimeter_2d = function(x) {
 	if (isTRUE(st_is_longlat(x)))
 		stop("for perimeter of longlat geometry, cast to LINESTRING and use st_length") # nocov
